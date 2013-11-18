6f9aae851619b2b74d86de2fe8653be03561bd49
Robrecht Dewaele <rdewaele@infogroep.be>
2013-11-18 15:58:00 +0100

    edge case bugfix in bytes prettyprint

--- benchmarks/arraywalk/prettyprint.cpp
+++ benchmarks/arraywalk/prettyprint.cpp
@@ -3,24 +3,27 @@
 using namespace std;
 
 namespace prettyprint {
+
 	ostream & operator<<(ostream & out, const Bytes & b) {
-		struct {
+		static const struct {
 			uint64_t bound;
 			string suffix;
 		} table[] = {
-			{ (uint64_t) 1 << 10, "B" },
-			{ (uint64_t) 1 << 20, "KiB" },
-			{ (uint64_t) 1 << 30, "MiB" },
-			{ (uint64_t) 1 << 40, "GiB" },
-			{ (uint64_t) 1 << 50, "TiB" },
-			{ (uint64_t) 1 << 60, "PiB" }
+			{ (uint64_t) 1 << 60, "EiB" },
+			{ (uint64_t) 1 << 50, "PiB" },
+			{ (uint64_t) 1 << 40, "TiB" },
+			{ (uint64_t) 1 << 30, "GiB" },
+			{ (uint64_t) 1 << 20, "MiB" },
+			{ (uint64_t) 1 << 10, "KiB" },
+			{ (uint64_t) 0, "B" },
 		};
 		for (auto & row : table)
-			if (b.bytes < row.bound)
-				return out << (double) b.bytes / (double) (row.bound >> 10)
+			if (b.bytes >= row.bound)
+				return out << (double) b.bytes / (double) row.bound
 					<< " " << row.suffix;
 
 		// unreachable - suppress "control reaches end of non-void function"
 		return out;
 	}
+
 }
