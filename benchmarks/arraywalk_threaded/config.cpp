ad53e110c8159f34b8035b1a79bb96e8ce7f2252
Robrecht Dewaele <rdewaele@vub.ac.be>
2014-02-11 19:03:54 +0100

    threaded arraywalk array size is now a range with custom increment TODO: allow this type to be used in RangeSet, currently it only uses Range

--- benchmarks/arraywalk_threaded/config.cpp
+++ benchmarks/arraywalk_threaded/config.cpp
@@ -12,12 +12,10 @@ namespace arraywalk {
 			size_t _size_inc, unsigned _istream_min, unsigned _istream_max,
 			uintptr_t _align, pattern _ptrn, uint_fast32_t _MiB):
 		RangeSet(
-				Range<size_t>(_size_min, _size_max),
+				ArraySize(_size_min, _size_max, _size_inc, _size_mul),
 				Range<unsigned>(_istream_min, _istream_max)),
 		threads_min(_threads_min),
 		threads_max(_threads_max),
-		size_mul(_size_mul),
-		size_inc(_size_inc),
 		align(_align),
 		ptrn(_ptrn),
 		readMiB(_MiB)
--- benchmarks/arraywalk_threaded/config.hpp
+++ benchmarks/arraywalk_threaded/config.hpp
@@ -7,6 +7,8 @@
 #include <cstddef>
 #include <cstdint>
 
+#include <iostream>
+
 namespace arraywalk {
 
 	enum pattern { RANDOM, INCREASING, DECREASING };
@@ -16,9 +18,7 @@ namespace arraywalk {
 		static constexpr unsigned threads_max = 4;
 
 		static constexpr size_t size_min = 1 << 12;
-		// TODO: extend Range implementation so it can cope with custom increments
-		//static constexpr size_t size_max = 1 << 14;
-		static constexpr size_t size_max = 1 << 12;
+		static constexpr size_t size_max = 1 << 14;
 		static constexpr unsigned size_mul = 1;
 		static constexpr size_t size_inc = 1 << 12;
 
@@ -32,7 +32,24 @@ namespace arraywalk {
 		static constexpr uint_fast32_t MiB = 1 << 8;
 	}
 
+	class ArraySize: public adhd::Range<size_t> {
+		public:
+			ArraySize(size_t min, size_t max, size_t inc, unsigned mul = 1u)
+				: Range(min, max), mulval(mul), incval(inc)
+			{}
+
+			virtual inline size_t increment(size_t & value) override {
+				std::cerr << "HALLO DAAR" << std::endl;
+				return value * mulval + incval;
+			}
+
+		private:
+			const unsigned mulval;
+			const size_t incval;
+	};
+
 	struct Config: public adhd::RangeSet<size_t, unsigned> {
+
 		Config(
 				unsigned _threads_min = defaults::threads_min,
 				unsigned _threads_max = defaults::threads_max,
@@ -56,8 +73,6 @@ namespace arraywalk {
 
 		unsigned threads_min;
 		unsigned threads_max;
-		unsigned size_mul;
-		size_t size_inc;
 		uintptr_t align;
 		pattern ptrn;
 		uint_fast32_t readMiB;
