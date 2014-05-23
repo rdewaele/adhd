d6bcf325812e4a882c5d68f237d9f3e72bb49382
Robrecht Dewaele <rdewaele@vub.ac.be>
2014-05-23 15:16:52 +0200

    further simplified rdtsc(): always read 64-bit values

--- benchmarks/rdtsc.h
+++ benchmarks/rdtsc.h
@@ -2,20 +2,8 @@
 
 #include<stdint.h>
 
-#if defined(__i386__)
-
-static inline uint64_t rdtsc(void) {
-	uint64_t stamp;
-	asm volatile ("rdtsc":"=A"(stamp));
-	return stamp;
-}
-
-#elif defined(__x86_64__)
-
 static inline uint64_t rdtsc(void) {
 	uint32_t lo, hi;
 	asm volatile ("rdtsc":"=a"(lo),"=d"(hi));
 	return lo | ((uint64_t) hi << 32);
 }
-
-#endif
